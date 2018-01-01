b6321f246133b82ad8e0d33e4ceaa6305f7ac719
Nolan Byrnes <nolanehs@gmail.com>
2018-01-01 18:37:21 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 FROM users INNER JOIN pledges
 ON pledges.user_id = users.id
 GROUP BY(users.name)
-ORDER BY (pledges.amount, users.name) ASC "
+ORDER BY (pledges.amount) ASC "
 "
 end
 
